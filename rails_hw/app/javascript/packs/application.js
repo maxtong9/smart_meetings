03f9bf6a6743e289482fb25e976fff12a921b895
Tuan Le <tuanle@umail.ucsb.edu>
2019-10-22 11:37:26 -0700

    added react to rails, but component is not being rendered

--- rails_hw/app/javascript/packs/application.js
+++ rails_hw/app/javascript/packs/application.js
@@ -15,3 +15,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
+ReactRailsUJS.mountComponents();
