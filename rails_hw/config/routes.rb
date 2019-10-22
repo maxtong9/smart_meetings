916c54304f8ee234056272f192aaa065a9d14b3b
Tuan Le <tuanle@umail.ucsb.edu>
2019-10-22 14:44:15 -0700

    added more gems

--- rails_hw/config/routes.rb
+++ rails_hw/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   root 'static#index'
 	namespace :v1, defaults: { format: 'json' } do
      get 'things', to: 'things#index'
