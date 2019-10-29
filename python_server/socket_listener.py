29257142b06a416336075535b51ba7798bda736c
tuanle <tuanle@umail.ucsb.edu>
2019-10-29 14:22:58 -0700

    Merge branch 'rails_s3' into develop
    develop x rails_s3

---
+++ python_server/socket_listener.py
@@ -0,0 +1,22 @@
+#Socket Connection as well as Python access to S3
+import socketserver
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    # Create the server, binding to localhost on port 9999
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
