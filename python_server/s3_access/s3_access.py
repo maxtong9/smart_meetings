93f235c7f9e3184c3b6f62502279f96d47627900
jsmli <jsimingli@gmail.com>
2019-10-31 12:20:42 -0700

    upload files to s3 function

--- python_server/s3_access/s3_access.py
+++ python_server/s3_access/s3_access.py
@@ -6,35 +6,57 @@ from botocore.exceptions import ClientError
 
 ################## FILE DOWNLOAD ####################
 
-# Key and filename for testing download
-# bucket = 'smartmeetingsbelieving'
-# object_key = 'ts65ue95bv6vi70y3gnogbmcs6mz'
-# object_name = 'gordonBurger.txt'
-
 # Create directory for storing files temporarily to process
-directory = "/tmp/"
-os.makedirs(directory, exist_ok=True)
+directory = "./tmp/"
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
-def download_file_from_S3(bucket, object_key, object_name):
+def download_file_from_S3(bucket, file_name, object_key):
     """Download a file from an S3 bucket
 
     :param bucket: Bucket to download from
+    :param file_name: Local object name to download to
     :param object_key: Key of object to download
-    :param object_name: Local object name to download to
     :return: True if file was downloaded, else False
     """
-    s3 = boto3.resource('s3')
 
+    # Download the file
+    s3 = boto3.resource('s3')
     try:
-        response = s3.meta.client.download_file(bucket, object_key, directory + object_name)
+        response = s3.meta.client.download_file(bucket, object_key, directory+file_name)
     except ClientError as e:
         logging.error(e)
         return False
     return True
 
-# Function call for testing File Download
-# download_file_from_S3(file_key, file_name)
+################ FILE DOWNLOAD END ################
 
-############# FILE DOWNLOAD END ######################
+################ FILE UPLOAD ################
 
-######## FILE UPLOAD ####################
+# Adapted from AWS S3 documentation
+def upload_file_to_S3(bucket, file_name, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param bucket: Bucket to upload to
+    :param file_name: File to upload
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3 = boto3.resource('s3')
+    try:
+        response = s3.meta.client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    except FileNotFoundError as e:
+        logging.error(e)
+        return False
+    return True
+
+################ FILE UPLOAD END ################
