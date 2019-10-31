05daf99d2b91fa45c76cca7d455d6ce5e60869b9
Sarita Phoosopha <sarita_p@cs.ucsb.edu>
2019-10-31 14:14:27 -0700

    Update to Transcribe.py

--- python_server/main.py
+++ python_server/main.py
@@ -2,7 +2,7 @@ from transcribe import *
 from text_processor import *
 def main():
 	# Transcribe Audio
-	T = Transcribe("audio-file.flac")
+	T = Transcribe(["Sarita.wav", "Christina.wav"])
 	T.transcription()
 	# Used Audio in Text Processor
 	tp = TextProcessor("Hello, Input")
