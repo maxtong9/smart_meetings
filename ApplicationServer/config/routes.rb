ecdf8025d24410cab2cc017d73fdff2b701cad28
jsmli <jsimingli@gmail.com>
2019-11-21 11:27:31 -0800

    Update routes.rb

--- ApplicationServer/config/routes.rb
+++ ApplicationServer/config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   #get 'hello_world', to: 'hello_world#index'
   root 'hello_world#index'
   get 'about', to: 'about#index'
-  get 'meeting', to: 'meeting#index'
   #match '*path', to: 'hello_world#index' via :all
 end
