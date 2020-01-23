ecbcdeb885962795cf8c84961171cfa95558d3ae
jsmli <jsimingli@gmail.com>
2020-01-22 21:44:51 -0800

    sessions implementation init

---
+++ ApplicationServer/app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_name(params[:name])
+    binding.pry
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Logged in!"
+    else
+      flash.now[:alert] = "Email or password is invalid"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged out!"
+  end
+end
--- ApplicationServer/config/routes.rb
+++ ApplicationServer/config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
   root 'hello_world#index'
   get 'about', to: 'about#index'
   #match '*path', to: 'hello_world#index' via :all
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
 end
