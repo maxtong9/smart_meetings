e177abb93f1255cdfab0d26a39cd0ef9a85e2824
Jackson Li <jsimingli@gmail.com>
2020-02-18 19:21:30 -0800

    personalized emails and links

--- ApplicationServer/config/application.rb
+++ ApplicationServer/config/application.rb
@@ -11,6 +11,8 @@ module ApplicationServer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
