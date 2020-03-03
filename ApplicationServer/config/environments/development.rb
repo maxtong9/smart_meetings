64fd3f706a08109ec8af0f8eb16258571ee0212e
EC2 Default User <ec2-user@ip-172-31-12-227.us-west-1.compute.internal>
2020-03-03 19:21:16 +0000

    deployment changes

--- ApplicationServer/config/environments/development.rb
+++ ApplicationServer/config/environments/development.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.hosts << "ec2-54-183-181-240.us-west-1.compute.amazonaws.com"
   
 config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
